885019cdbf06a3711e13cbda8fbd21e443a43bc5
Aleksey Goncharov <to-english@yandex.ru>
2022-09-10 14:33:45 +0300

    Solved all the tasks for module-2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let resSum = 0;
+  if (!Object.keys(salaries).length) {
+    return resSum;
+  }
+  for (const salariesKey in salaries) {
+    if (isFinite(salaries[salariesKey])) {
+      resSum += salaries[salariesKey];
+    }
+  }
+  return resSum;
 }
