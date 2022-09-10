1b6354dc154749451bfce9c416ef61cade51384f
Aleksey Goncharov <to-english@yandex.ru>
2022-09-10 17:00:55 +0300

    Remove if-condition in task-1 module-2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,8 +1,5 @@
 function sumSalary(salaries) {
   let resSum = 0;
-  if (!Object.keys(salaries).length) {
-    return resSum;
-  }
   for (const salariesKey in salaries) {
     if (isFinite(salaries[salariesKey])) {
       resSum += salaries[salariesKey];
