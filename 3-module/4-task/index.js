50af6df68b46912b976d54eb94850b48df7433d7
Aleksey Goncharov <to-english@yandex.ru>
2022-09-14 12:29:36 +0300

    Fix line break in task 4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,6 +1,5 @@
 function showSalary(users, age) {
   return users.filter(elem => elem.age <= age)
-    .map(({name, balance}) => `${name}, ${balance}\n`)
-    .join('')
-    .trim();
+    .map(({name, balance}) => `${name}, ${balance}`)
+    .join('\n');
 }
